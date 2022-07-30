3f1bf10cfbb0baab61ac66f2f63cb517c9bdd6be
Vincent-Zhang4135 <65748334+Vincent-Zhang4135@users.noreply.github.com>
2022-07-29 20:35:17 -0400

    progress

---
+++ personal-website/src/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const Hero = (props: any): JSX.Element => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Hero;
